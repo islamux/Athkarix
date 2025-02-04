e0c4de76ccfa41b793214505ff93fd9fb4c461bb
islamux <fathi733@gmail.com>
2025-02-04 11:39:30 +0300

    fix: erro that make footer shown in the next, dua_men_quran_list__model

--- lib/core/data/model/model_list/dua_men_quran_list.dart
+++ lib/core/data/model/model_list/dua_men_quran_list.dart
@@ -2,28 +2,39 @@ import 'package:athkarix/core/data/model/athkar_model.dart';
 import 'package:athkarix/core/data/static/text/dua_men_quran_text.dart';
 
 List<AthkarModel> duaMenQuranList = [
-  AthkarModel(duaText: DuaMenQuranText.duamenquran1),
-  AthkarModel(footer: DuaMenQuranText.duamenquran1_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran2),
-  AthkarModel(footer: DuaMenQuranText.duamenquran2_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran3),
-  AthkarModel(footer: DuaMenQuranText.duamenquran3_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran4),
-  AthkarModel(footer: DuaMenQuranText.duamenquran4_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran5),
-  AthkarModel(footer: DuaMenQuranText.duamenquran5_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran6),
-  AthkarModel(footer: DuaMenQuranText.duamenquran6_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran7),
-  AthkarModel(footer: DuaMenQuranText.duamenquran7_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran8),
-  AthkarModel(footer: DuaMenQuranText.duamenquran8_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran9),
-  AthkarModel(footer: DuaMenQuranText.duamenquran9_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran10),
-  AthkarModel(footer: DuaMenQuranText.duamenquran10_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran11),
-  AthkarModel(footer: DuaMenQuranText.duamenquran11_Footer),
-  AthkarModel(duaText: DuaMenQuranText.duamenquran12),
-  AthkarModel(footer: DuaMenQuranText.duamenquran12_Footer),
+  AthkarModel(duaText: DuaMenQuranText.duamenquran1,
+  footer: DuaMenQuranText.duamenquran1_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran2,
+  footer: DuaMenQuranText.duamenquran2_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran3,
+  footer: DuaMenQuranText.duamenquran3_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran4,
+  footer: DuaMenQuranText.duamenquran4_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran5,
+  footer: DuaMenQuranText.duamenquran5_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran6,
+  footer: DuaMenQuranText.duamenquran6_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran7,
+  footer: DuaMenQuranText.duamenquran7_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran8,
+  footer: DuaMenQuranText.duamenquran8_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran9,
+  footer: DuaMenQuranText.duamenquran9_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran10,
+  footer: DuaMenQuranText.duamenquran10_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran11,
+  footer: DuaMenQuranText.duamenquran11_Footer),
+  //
+  AthkarModel(duaText: DuaMenQuranText.duamenquran12,
+  footer: DuaMenQuranText.duamenquran12_Footer),
 ];
