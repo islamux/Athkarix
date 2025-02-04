d9d08e11f94ce2929261c6a61e4503ce4fd0cf05
islamux <fathi733@gmail.com>
2025-02-04 11:46:12 +0300

    fix: erro that make footer shown in the next, dua_men_sunnah_list__model

--- lib/core/data/model/model_list/estigfar_list_model.dart
+++ lib/core/data/model/model_list/estigfar_list_model.dart
@@ -6,41 +6,60 @@ List<AthkarModel> estigfarList = [
   AthkarModel(duaText: EstigfarText.estigfar1),
   AthkarModel(duaText: EstigfarText.estigfar2),
   AthkarModel(duaText: EstigfarText.estigfar3),
-  AthkarModel(duaText: EstigfarText.estigfar4),
-  AthkarModel(footer: EstigfarText.estigfar4_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar5),
-  AthkarModel(footer: EstigfarText.estigfar5_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar4, footer: EstigfarText.estigfar4_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar5, footer: EstigfarText.estigfar5_Footer),
+  //
   AthkarModel(duaText: EstigfarText.estigfar6),
   AthkarModel(duaText: EstigfarText.estigfar23),
   AthkarModel(duaText: EstigfarText.estigfar7),
-  AthkarModel(duaText: EstigfarText.estigfar8),
-  AthkarModel(footer: EstigfarText.estigfar8_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar8, footer: EstigfarText.estigfar8_Footer),
+  //
   AthkarModel(duaText: EstigfarText.estigfar9),
-  AthkarModel(duaText: EstigfarText.estigfar10),
-  AthkarModel(footer: EstigfarText.estigfar10_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar11),
-  AthkarModel(footer: EstigfarText.estigfar11_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar10, footer: EstigfarText.estigfar10_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar11, footer: EstigfarText.estigfar11_Footer),
+  //
   AthkarModel(duaText: EstigfarText.estigfar12),
-  AthkarModel(duaText: EstigfarText.estigfar13),
-  AthkarModel(footer: EstigfarText.estigfar13_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar14),
-  AthkarModel(footer: EstigfarText.estigfar14_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar15),
-  AthkarModel(footer: EstigfarText.estigfar15_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar16),
-  AthkarModel(footer: EstigfarText.estigfar16_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar17),
-  AthkarModel(footer: EstigfarText.estigfar17_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar18),
-  AthkarModel(footer: EstigfarText.estigfar18_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar22),
-  AthkarModel(footer: EstigfarText.estigfar22_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar24),
-  AthkarModel(footer: EstigfarText.estigfar24_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar25),
-  AthkarModel(footer: EstigfarText.estigfar25_Footer),
-  AthkarModel(duaText: EstigfarText.estigfar26),
-  AthkarModel(footer: EstigfarText.estigfar26_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar13, footer: EstigfarText.estigfar13_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar14, footer: EstigfarText.estigfar14_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar15, footer: EstigfarText.estigfar15_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar16, footer: EstigfarText.estigfar16_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar17, footer: EstigfarText.estigfar17_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar18, footer: EstigfarText.estigfar18_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar22, footer: EstigfarText.estigfar22_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar24, footer: EstigfarText.estigfar24_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar25, footer: EstigfarText.estigfar25_Footer),
+  //
+  AthkarModel(
+      duaText: EstigfarText.estigfar26, footer: EstigfarText.estigfar26_Footer),
+  //
   AthkarModel(duaText: EstigfarText.estigfar20),
   AthkarModel(duaText: EstigfarText.estigfar21),
 ];
