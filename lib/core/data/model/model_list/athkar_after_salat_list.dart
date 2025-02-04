90fde79c364e8f64344cbf7ef907dff583540dd1
islamux <fathi733@gmail.com>
2025-02-04 11:31:22 +0300

    fix: erro that make footer shown in the next, athkar_after_salat__model

--- lib/core/data/model/model_list/athkar_after_salat_list.dart
+++ lib/core/data/model/model_list/athkar_after_salat_list.dart
@@ -6,17 +6,27 @@ List<AthkarModel> athkarAfterSalatList = [
   AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat2),
   AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat33),
   AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat3),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat3),
   AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat4),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat5),
-  AthkarModel(footer: AthkarAfterSalatText.athkarAfterSalat5_Footer),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat6),
-  AthkarModel(footer: AthkarAfterSalatText.athkarAfterSalat6_Footer),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat7),
-  AthkarModel(footer: AthkarAfterSalatText.athkarAfterSalat7_Footer),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat8),
-  AthkarModel(footer: AthkarAfterSalatText.athkarAfterSalat8_Footer),
+  //
+  AthkarModel(
+      duaText: AthkarAfterSalatText.athkarAfterSalat5,
+      footer: AthkarAfterSalatText.athkarAfterSalat5_Footer),
+  //
+  AthkarModel(
+      duaText: AthkarAfterSalatText.athkarAfterSalat6,
+      footer: AthkarAfterSalatText.athkarAfterSalat6_Footer),
+  //
+  AthkarModel(
+      duaText: AthkarAfterSalatText.athkarAfterSalat7,
+      footer: AthkarAfterSalatText.athkarAfterSalat7_Footer),
+  //
+  AthkarModel(
+      duaText: AthkarAfterSalatText.athkarAfterSalat8,
+      footer: AthkarAfterSalatText.athkarAfterSalat8_Footer),
+  //
   AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat9),
-  AthkarModel(duaText: AthkarAfterSalatText.athkarAfterSalat10),
-  AthkarModel(footer: AthkarAfterSalatText.athkarAfterSalat10_Footer),
+  //
+  AthkarModel(
+      duaText: AthkarAfterSalatText.athkarAfterSalat10,
+      footer: AthkarAfterSalatText.athkarAfterSalat10_Footer),
 ];
