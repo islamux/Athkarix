bfc43c6eaeaad012c6ad0e559be9e53595ed04cd
islamux <fathi733@gmail.com>
2025-02-04 11:44:09 +0300

    fix: erro that make footer shown in the next, dua_men_sunnah_list__model

--- lib/core/data/model/model_list/dua_men_sunnah_list.dart
+++ lib/core/data/model/model_list/dua_men_sunnah_list.dart
@@ -4,88 +4,173 @@ import 'package:athkarix/core/data/static/text/dua_men_sunnah_text.dart';
 //:%s/\(duaText:\s*\)\(.*_Footer\)/footer: \2/g
 
 List<AthkarModel> duaMenSunnahList = [
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah13),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah13_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah14),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah14_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah15),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah15_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah16),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah16_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah17),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah17_Footer),
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah13,
+      footer: DuaMenSunnahText.duamensunnah13_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah14,
+      footer: DuaMenSunnahText.duamensunnah14_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah15,
+      footer: DuaMenSunnahText.duamensunnah15_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah16,
+      footer: DuaMenSunnahText.duamensunnah16_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah17,
+      footer: DuaMenSunnahText.duamensunnah17_Footer),
+  //
   AthkarModel(duaText: DuaMenSunnahText.duamensunnah18),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah19),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah19_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah20),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah20_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah21),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah21_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah22),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah22_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah24),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah24_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah25),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah25_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah26),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah26_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah27),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah27_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah28),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah28_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah29),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah29_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah31),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah31_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah32),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah32_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah34),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah34_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah35),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah35_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah36),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah36_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah37),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah37_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah38),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah38_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah39),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah39_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah40),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah40_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah41),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah41_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah42),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah42_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah19,
+      footer: DuaMenSunnahText.duamensunnah19_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah20,
+      footer: DuaMenSunnahText.duamensunnah20_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah21,
+      footer: DuaMenSunnahText.duamensunnah21_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah22,
+      footer: DuaMenSunnahText.duamensunnah22_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah24,
+      footer: DuaMenSunnahText.duamensunnah24_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah25,
+      footer: DuaMenSunnahText.duamensunnah25_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah26,
+      footer: DuaMenSunnahText.duamensunnah26_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah27,
+      footer: DuaMenSunnahText.duamensunnah27_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah28,
+      footer: DuaMenSunnahText.duamensunnah28_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah29,
+      footer: DuaMenSunnahText.duamensunnah29_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah31,
+      footer: DuaMenSunnahText.duamensunnah31_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah32,
+      footer: DuaMenSunnahText.duamensunnah32_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah34,
+      footer: DuaMenSunnahText.duamensunnah34_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah35,
+      footer: DuaMenSunnahText.duamensunnah35_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah36,
+      footer: DuaMenSunnahText.duamensunnah36_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah37,
+      footer: DuaMenSunnahText.duamensunnah37_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah38,
+      footer: DuaMenSunnahText.duamensunnah38_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah39,
+      footer: DuaMenSunnahText.duamensunnah39_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah40,
+      footer: DuaMenSunnahText.duamensunnah40_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah41,
+      footer: DuaMenSunnahText.duamensunnah41_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah42,
+      footer: DuaMenSunnahText.duamensunnah42_Footer),
+  //
   AthkarModel(duaText: DuaMenSunnahText.duamensunnah43),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah45),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah45_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah46),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah46_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah47),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah47_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah48),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah48_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah49),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah49_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah45,
+      footer: DuaMenSunnahText.duamensunnah45_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah46,
+      footer: DuaMenSunnahText.duamensunnah46_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah47,
+      footer: DuaMenSunnahText.duamensunnah47_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah48,
+      footer: DuaMenSunnahText.duamensunnah48_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah49,
+      footer: DuaMenSunnahText.duamensunnah49_Footer),
+  //
   AthkarModel(duaText: DuaMenSunnahText.duamensunnah50),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah51),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah51_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah52),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah52_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah53),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah53_Footer),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah54_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah55),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah55_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah56),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah56_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah57),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah57_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah58),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah58_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah59),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah59_Footer),
-  AthkarModel(duaText: DuaMenSunnahText.duamensunnah60),
-  AthkarModel(footer: DuaMenSunnahText.duamensunnah60_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah51,
+      footer: DuaMenSunnahText.duamensunnah51_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah52,
+      footer: DuaMenSunnahText.duamensunnah52_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah53,
+      footer: DuaMenSunnahText.duamensunnah53_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah54,
+      footer: DuaMenSunnahText.duamensunnah54_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah55,
+      footer: DuaMenSunnahText.duamensunnah55_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah56,
+      footer: DuaMenSunnahText.duamensunnah56_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah57,
+      footer: DuaMenSunnahText.duamensunnah57_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah58,
+      footer: DuaMenSunnahText.duamensunnah58_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah59,
+      footer: DuaMenSunnahText.duamensunnah59_Footer),
+  //
+  AthkarModel(
+      duaText: DuaMenSunnahText.duamensunnah60,
+      footer: DuaMenSunnahText.duamensunnah60_Footer),
 ];
