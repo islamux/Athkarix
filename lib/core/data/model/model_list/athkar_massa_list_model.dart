5efd8fefd8023f18b612c9815bd95f939b0cff2b
islamux <fathi733@gmail.com>
2025-02-04 11:37:32 +0300

    fix: erro that make footer shown in the next, athkar_massa_salat__model

--- lib/core/data/model/model_list/athkar_massa_list_model.dart
+++ lib/core/data/model/model_list/athkar_massa_list_model.dart
@@ -3,54 +3,90 @@ import 'package:athkarix/core/data/static/text/ahkar_massa_text.dart';
 
 List<AthkarModel> athkarMassaList = [
   AthkarModel(
-    duaText: AthkarMassaText.athkarMassa1,
-  ), // الحمدلله وحده
-  AthkarModel(footer: AthkarMassaText.athkarMassa1_Footer), // الحمدلله وحده
-  AthkarModel(duaText: AthkarMassaText.athkarMassa111), // الكرسي
-  AthkarModel(footer: AthkarMassaText.athkarMassa111_Footer), // الكرسي
-  AthkarModel(duaText: AthkarMassaText.athkarMassa11), // المعوذات
-  AthkarModel(footer: AthkarMassaText.athkarMassa11_Footer), // المعوذات
-  AthkarModel(duaText: AthkarMassaText.athkarMassa222), // الملك لله والحمدلله
+      duaText: AthkarMassaText.athkarMassa1,
+      footer: AthkarMassaText.athkarMassa1_Footer), // الحمدلله وحده
+  //
   AthkarModel(
+      duaText: AthkarMassaText.athkarMassa111, // الكرسي
+      footer: AthkarMassaText.athkarMassa111_Footer), // الكرسي
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa11, // المعوذات
+      footer: AthkarMassaText.athkarMassa11_Footer), // المعوذات
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa222, // الملك لله والحمدلله
       footer: AthkarMassaText.athkarMassa222_Footer), // الملك لله والحمدلله
-  AthkarModel(duaText: AthkarMassaText.athkarMassa2), // اللهم بك أمسينا
-  AthkarModel(footer: AthkarMassaText.athkarMassa2_Footer), // اللهم بك أمسينا
-  AthkarModel(duaText: AthkarMassaText.athkarMassa22), // سيد الاستغفار
-  AthkarModel(footer: AthkarMassaText.athkarMassa22_Footer), // سيد الاستغفار
-  AthkarModel(duaText: AthkarMassaText.athkarMassa3), // امسيت اشهدك
-  AthkarModel(footer: AthkarMassaText.athkarMassa3_Footer), // امسيت اشهدك
-  AthkarModel(duaText: AthkarMassaText.athkarMassa4), // ما امسى بي من نعمة
+  //
   AthkarModel(
+      duaText: AthkarMassaText.athkarMassa2, // اللهم بك أمسينا
+      footer: AthkarMassaText.athkarMassa2_Footer), // اللهم بك أمسينا
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa22, // سيد الاستغفار
+      footer: AthkarMassaText.athkarMassa22_Footer), // سيد الاستغفار
+
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa3, // امسيت اشهدك
+      footer: AthkarMassaText.athkarMassa3_Footer), // امسيت اشهدك
+
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa4, // ما امسى بي من نعمة
       footer: AthkarMassaText.athkarMassa4_Footer), // ما امسى بي من نعمة
-  AthkarModel(duaText: AthkarMassaText.athkarMassa44), // عافني في بدني
-  AthkarModel(footer: AthkarMassaText.athkarMassa44_Footer), // عافني في بدني
-  AthkarModel(duaText: AthkarMassaText.athkarMassa5), // حسبي الله
-  AthkarModel(footer: AthkarMassaText.athkarMassa5_Footer), // حسبي الله
-  AthkarModel(duaText: AthkarMassaText.athkarMassa55), // العفو والعافية في
+  //
   AthkarModel(
+      duaText: AthkarMassaText.athkarMassa44, // عافني في بدني
+      footer: AthkarMassaText.athkarMassa44_Footer), // عافني في بدني
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa5, // حسبي الله
+      footer: AthkarMassaText.athkarMassa5_Footer), // حسبي الله
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa55, // العفو والعافية في
       footer: AthkarMassaText.athkarMassa55_Footer), // العفو والعافية في
-  AthkarModel(duaText: AthkarMassaText.athkarMassa6), // عالم الغيب
-  AthkarModel(footer: AthkarMassaText.athkarMassa6_Footer), // عالم الغيب
-  AthkarModel(duaText: AthkarMassaText.athkarMassa66), // بسم الله الذي
-  AthkarModel(footer: AthkarMassaText.athkarMassa66_Footer), // بسم الله الذي
-  AthkarModel(duaText: AthkarMassaText.athkarMassa7), // رضيت
-  AthkarModel(footer: AthkarMassaText.athkarMassa7_Footer), // رضيت
-  AthkarModel(duaText: AthkarMassaText.athkarMassa8), // ياحي ياقيوم
-  AthkarModel(footer: AthkarMassaText.athkarMassa8_Footer), // ياحي ياقيوم
-  AthkarModel(duaText: AthkarMassaText.athkarMassa9), // امسينا وامسى الملك لله
+  //
   AthkarModel(
+      duaText: AthkarMassaText.athkarMassa6, // عالم الغيب
+      footer: AthkarMassaText.athkarMassa6_Footer), // عالم الغيب
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa66, // بسم الله الذي
+      footer: AthkarMassaText.athkarMassa66_Footer), // بسم الله الذي
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa7, // رضيت
+      footer: AthkarMassaText.athkarMassa7_Footer), // رضيت
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa8, // ياحي ياقيوم
+      footer: AthkarMassaText.athkarMassa8_Footer), // ياحي ياقيوم
+//
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa9, // امسينا وامسى الملك لله
       footer: AthkarMassaText.athkarMassa9_Footer), // امسينا وامسى الملك لله
-  AthkarModel(duaText: AthkarMassaText.athkarMassa10), // امسينا على فطرة
-  AthkarModel(footer: AthkarMassaText.athkarMassa10_Footer), // امسينا على فطرة
-  AthkarModel(duaText: AthkarMassaText.athkarMassa1111), // سبحان الله وبحمده
+  //
   AthkarModel(
+      duaText: AthkarMassaText.athkarMassa10, // امسينا على فطرة
+      footer: AthkarMassaText.athkarMassa10_Footer), // امسينا على فطرة
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa1111, // سبحان الله وبحمده
       footer: AthkarMassaText.athkarMassa1111_Footer), // سبحان الله وبحمده
-  AthkarModel(duaText: AthkarMassaText.athkarMassa12), // لا اله الا الله
-  AthkarModel(footer: AthkarMassaText.athkarMassa12_Footer), // لا اله الا الله
-  AthkarModel(duaText: AthkarMassaText.athkarMassa16), // استغفر الله
-  AthkarModel(footer: AthkarMassaText.athkarMassa16_Footer), // استغفر الله
-  AthkarModel(duaText: AthkarMassaText.athkarMassa17), // أعوذ بكلمات
-  AthkarModel(footer: AthkarMassaText.athkarMassa17_Footer), // أعوذ بكلمات
-  AthkarModel(duaText: AthkarMassaText.athkarMassa18), // اللهم صل وسلم
-  AthkarModel(footer: AthkarMassaText.athkarMassa18_Footer), // اللهم صل وسلم
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa12, // لا اله الا الله
+      footer: AthkarMassaText.athkarMassa12_Footer), // لا اله الا الله
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa16, // استغفر الله
+      footer: AthkarMassaText.athkarMassa16_Footer), // استغفر الله
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa17, // أعوذ بكلمات
+      footer: AthkarMassaText.athkarMassa17_Footer), // أعوذ بكلمات
+  //
+  AthkarModel(
+      duaText: AthkarMassaText.athkarMassa18, // اللهم صل وسلم
+      footer: AthkarMassaText.athkarMassa18_Footer), // اللهم صل وسلم
 ];
